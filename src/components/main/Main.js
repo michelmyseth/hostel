cbe22d3fe1f3ab51a65f9d5c8e678201c595d4fb
michelmyseth <38660333+michelmyseth@users.noreply.github.com>
2022-04-15 00:35:21 +0200

    fix front nav bar

--- src/components/main/Main.js
+++ src/components/main/Main.js
@@ -1,27 +1,28 @@
-import React from 'react'
-import './MainStyles.css'
-import {AiOutlineSearch} from 'react-icons/ai'
-import FrontImageMain from '../../assets/123.jpeg'
-
+import React from 'react';
+import './MainStyles.css';
+import { AiOutlineSearch } from 'react-icons/ai';
+import FrontImageMain from '../../assets/123.jpeg';
 
 function Main() {
     return (
-        <div className='main'>
-            <img src={FrontImageMain} id='image' alt='/'/>
-            <div className='content'>
-            <h1>First Hostel Travel</h1>
-            <h2>Top 1 Locations WorldWide</h2>
-            <form className='form'>
-                <div>
-                    <input type='text' placeholder='Search Hostel'/>
-                </div>
-                <div>
-                    <button><AiOutlineSearch className='icon'/></button>
-                </div>
-            </form>
+        <div className="main">
+            <img src={FrontImageMain} id="image" alt="/" />
+            <div className="content">
+                <h1>First Hostel Travel</h1>
+                <h2>Top 1 Locations WorldWide</h2>
+                <form className="form">
+                    <div>
+                        <input type="text" placeholder="Search Hostel" />
+                    </div>
+                    <div>
+                        <button>
+                            <AiOutlineSearch className="icon" />
+                        </button>
+                    </div>
+                </form>
             </div>
         </div>
-    )
+    );
 }
 
-export default Main
+export default Main;
